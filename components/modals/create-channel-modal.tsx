d59592d38005225382dec14fbd9aa45f7cdde50d
kyle-233 <lubingfu@chinabzc.com>
2024-07-19 17:31:20 +0800

    feat: add server sidebar channel item and add delete channel modal

--- components/modals/create-channel-modal.tsx
+++ components/modals/create-channel-modal.tsx
@@ -32,6 +32,7 @@ import {
   SelectTrigger,
   SelectValue,
 } from '../ui/select'
+import { useEffect } from 'react'
 
 const formSchema = z.object({
   name: z
@@ -46,18 +47,27 @@ const formSchema = z.object({
 })
 
 export const CreateChannelModal = () => {
-  const { isOpen, onClose, type } = useModal()
+  const { isOpen, onClose, type, data } = useModal()
   const router = useRouter()
   const params = useParams()
 
   const isModalOpen = isOpen && type === 'createChannel'
+  const { channelType } = data
   const form = useForm({
     resolver: zodResolver(formSchema),
     defaultValues: {
       name: '',
-      type: ChannelType.TEXT,
+      type: channelType || ChannelType.TEXT,
     },
   })
+
+  useEffect(() => {
+    if (channelType) {
+      form.setValue('type', channelType)
+    } else {
+      form.setValue('type', ChannelType.TEXT)
+    }
+  }, [channelType, form])
   const isLoading = form.formState.isSubmitting
 
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -1,5 +1,6 @@
 import { CreateChannelModal } from '../modals/create-channel-modal'
 import { CreateServerModal } from '../modals/create-server-modal'
+import { DeleteChannelModal } from '../modals/delete-channel-modal'
 import { DeleteServerModal } from '../modals/delete-server-modal'
 import { EditServerModal } from '../modals/edit-server-modal'
 import { InviteModal } from '../modals/invite-modal'
@@ -16,6 +17,7 @@ export const ModalProvider = () => {
       <CreateChannelModal />
       <LeaveServerModal />
       <DeleteServerModal />
+      <DeleteChannelModal />
     </>
   )
 }
