dda17b2175eac53d13a06f468a3c70a205526b98
kyle-233 <lubingfu@chinabzc.com>
2024-07-22 11:42:33 +0800

    fix: use "w-0" fixed sidebar hidden on "md screen"

--- components/mobile-toggle.tsx
+++ components/mobile-toggle.tsx
@@ -8,7 +8,9 @@ export const MobileToggle = ({ serverId }: { serverId: string }) => {
   return (
     <Sheet>
       <SheetTrigger asChild>
-        <Button variant="ghost" size="icon" className="md:hidden"></Button>
+        <Button variant="ghost" size="icon" className="md:hidden">
+          <Menu />
+        </Button>
       </SheetTrigger>
       <SheetContent side="left" className="p-0 flex gap-0">
         <div className="w-[72px]">
