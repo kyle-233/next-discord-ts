d757262d06955a824b227d3a6152b43aacbe4788
kyle-233 <lubingfu@chinabzc.com>
2024-07-23 17:21:55 +0800

    feat: add message item show message and edit send message

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import { ModalProvider } from '@/components/providers/modal-provider'
 
 import './globals.css'
 import { SocketProvider } from '@/components/providers/socket-provider'
+import { QueryProvider } from '@/components/providers/query-provider'
 const font = Open_Sans({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
@@ -31,7 +32,7 @@ export default function RootLayout({
           >
             <SocketProvider>
               <ModalProvider />
-              {children}
+              <QueryProvider>{children}</QueryProvider>
             </SocketProvider>
           </ThemeProvider>
         </body>
