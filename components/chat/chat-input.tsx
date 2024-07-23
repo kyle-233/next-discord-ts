1def32de3f01e43a4b0389aef847080459f66282
kyle-233 <lubingfu@chinabzc.com>
2024-07-23 14:21:28 +0800

    feat: add emoji popover

--- components/chat/chat-input.tsx
+++ components/chat/chat-input.tsx
@@ -9,6 +9,8 @@ import { Form, FormControl, FormField, FormItem } from '../ui/form'
 import { Plus, Smile } from 'lucide-react'
 import { Input } from '../ui/input'
 import { useModal } from '@/hooks/use-modal-store'
+import { EmojiPicker } from '../emoji-picker'
+import { useRouter } from 'next/navigation'
 
 interface ChatInputProps {
   apiUrl: string
@@ -22,6 +24,7 @@ const formSchema = z.object({
 })
 export const ChatInput = ({ apiUrl, query, name, type }: ChatInputProps) => {
   const { onOpen } = useModal()
+  const router = useRouter()
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -38,6 +41,8 @@ export const ChatInput = ({ apiUrl, query, name, type }: ChatInputProps) => {
         query,
       })
       await fetch(url, { method: 'POST', body: JSON.stringify(values) })
+      form.reset()
+      router.refresh()
     } catch (e) {
       console.log(e)
     }
@@ -66,7 +71,11 @@ export const ChatInput = ({ apiUrl, query, name, type }: ChatInputProps) => {
                     {...field}
                   />
                   <div className="absolute top-7 right-8">
-                    <Smile />
+                    <EmojiPicker
+                      onChange={(emoji: string) =>
+                        field.onChange(`${field.value} ${emoji}`)
+                      }
+                    />
                   </div>
                 </div>
               </FormControl>
